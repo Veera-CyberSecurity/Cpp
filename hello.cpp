73bebccb864c96348141594459610940db121298
Veera-CyberSecurity <veeranarayanavemula111@gmail.com>
2025-03-10 14:22:58 +0000

    written_comment_for_namespace

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+using namespace std; //This is namespace
 int main(){ //this is main function
 	cout << "Hello world"<<endl;  //This is hello world programme
 }
